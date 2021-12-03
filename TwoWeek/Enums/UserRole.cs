51ce1958f56e4119550360a52751680831b252fa
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-03 23:32:22 +0300

    Enums

---
+++ TwoWeek/Enums/UserRole.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+
+namespace TwoWeek.Enums
+{
+    public enum UserRole
+    {
+        None,
+        Admin,
+        Master
+    }
+}
