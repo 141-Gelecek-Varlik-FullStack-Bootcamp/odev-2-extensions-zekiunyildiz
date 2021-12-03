dd7359f7a03cadfa65c5622da9488b1a7d3fc95e
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-03 23:25:35 +0300

    Business

---
+++ TwoWeek/Business/Lesson.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace TwoWeek.Business
+{
+    [RoleAccess(UserRole.Admin)]
+    [RoleAccess(UserRole.Master)]
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private User User;
+
+        public Lesson(int id, string name, User user)
+        {
+            var typeInfo = typeof(Lesson);
+            RoleAccessAttribute[] roleAttributes = 
+                typeInfo.GetCustomAttributes(typeof(RoleAccessAttribute), true) 
+                as RoleAccessAttribute[];
+
+            if (roleAttributes != null)
+            {
+                if (roleAttributes.All(ra => ra.UserRole.ToString() != 
+                   user.GetType().Name))
+                {
+                    throw new ArgumentException("Kullanıcı yeterli yetkiye sahip değil");
+                }
+            }
+
+            this.Id = id;
+            this.Name = name;
+            this.User = user;
+
+        }
+    }
+}
