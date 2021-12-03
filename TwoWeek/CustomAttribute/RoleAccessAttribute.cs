3577be266486617df5271bb02b28ba9a1a80bced
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-03 23:29:39 +0300

    CustomAttribute

---
+++ TwoWeek/CustomAttribute/RoleAccessAttribute.cs
@@ -0,0 +1,19 @@
+using Bootcamp.Attributes_Reflection.Enums;
+using System;
+
+namespace TwoWeek.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | 
+        AttributeTargets.Constructor | 
+        AttributeTargets.Method, AllowMultiple = true)]
+    public class RoleAccessAttribute : Attribute
+    {
+        public UserRole UserRole { get; set; }
+
+        public RoleAccessAttribute(UserRole role)
+        {
+            UserRole = role;
+        }
+
+    }
+}
