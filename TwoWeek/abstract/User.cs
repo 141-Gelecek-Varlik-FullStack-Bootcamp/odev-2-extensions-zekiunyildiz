4a873c56dc0d0504f97924d18d780b3858cf51f4
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-03 23:19:00 +0300

    Abstract

---
+++ TwoWeek/abstract/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoWeek
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
